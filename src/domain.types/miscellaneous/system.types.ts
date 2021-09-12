9c352df7ea78f465be8317ec06dda7bdc2668e72
Kiran Kharade <kiran.kharade@reanfoundation.org>
2021-09-12 16:26:05 +0530

    Added allergy types

--- src/domain.types/miscellaneous/system.types.ts
+++ src/domain.types/miscellaneous/system.types.ts
@@ -5,4 +5,6 @@ export type Optional<T> = T | NotThere;
 export type Gender = 'Male'| 'male' | 'Female' | 'female' | 'Other' | 'other' |'Unknown' | 'unknown' | null;
 export type BloodGroup = 'A+'| 'B+' | 'O+' | 'AB+' | 'A-' | 'B-' |'O-' | 'AB-' | null;
 export type MaritalStatus = 'Single'| 'Married' | 'Widowed' | 'Divorcee' | 'Live-in' | 'Other' | 'Unknown' | null;
+
+//Generic severities
 export type Severity = 'Low'| 'Medium' | 'High' | 'Critical' | null;

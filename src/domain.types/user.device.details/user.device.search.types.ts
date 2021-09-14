1de619034c8904c48e7dca70b09bd9836b84dfd4
Kiran Kharade <kiran.kharade@reanfoundation.org>
2021-09-14 09:53:13 +0530

    Added user device details types

---
+++ src/domain.types/user.device.details/user.device.search.types.ts
@@ -0,0 +1,26 @@
+import { UserDeviceDetailsDto } from "./user.device.dto";
+
+//////////////////////////////////////////////////////////////////////
+
+export interface UserDeviceDetailsSearchFilters {
+    UserId?: string;
+    DeviceName?: string;
+    OSType?: string;
+    OSVersion?: string;
+    AppName?: string;
+    AppVersion?: string;
+    OrderBy: string;
+    Order: string;
+    PageIndex: number;
+    ItemsPerPage: number;
+}
+
+export interface UserDeviceDetailsSearchResults {
+    TotalCount: number;
+    RetrievedCount: number;
+    PageIndex: number;
+    ItemsPerPage: number;
+    Order: string;
+    OrderedBy: string;
+    Items: UserDeviceDetailsDto[];
+}

216e6af5c0ca3ccbd9a2aa35a8bd86a3255e0909
Sean White <swhite51096@gmail.com>
2021-09-14 06:47:17 -0400

    Created paths for blood pressure resource.

---
+++ src/services/blood.pressure.service.ts
@@ -0,0 +1,39 @@
+import { inject, injectable } from "tsyringe";
+import { IBloodPressureRepo } from "../database/repository.interfaces/blood.pressure.repo.interface";
+import { BloodPressureDomainModel } from '../domain.types/biometrics/blood.pressure/blood.pressure.domain.model';
+import { BloodPressureDto } from '../domain.types/biometrics/blood.pressure/blood.pressure.dto';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { BloodPressureSearchResults, BloodPressureSearchFilters } from '../domain.types/biometrics/blood.pressure/blood.pressure.search.types';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@injectable()
+export class BloodPressureService {
+
+    constructor(
+        @inject('IBloodPressureRepo') private _bloodPressureRepo: IBloodPressureRepo,
+    ) { }
+
+    create = async (bloodPressureDomainModel: BloodPressureDomainModel):
+    Promise<BloodPressureDto> => {
+        return await this._bloodPressureRepo.create(bloodPressureDomainModel);
+    };
+
+    getById = async (id: string): Promise<BloodPressureDto> => {
+        return await this._bloodPressureRepo.getById(id);
+    };
+
+    search = async (filters: BloodPressureSearchFilters): Promise<BloodPressureSearchResults> => {
+        return await this._bloodPressureRepo.search(filters);
+    };
+
+    update = async (id: string, bloodPressureDomainModel: BloodPressureDomainModel):
+    Promise<BloodPressureDto> => {
+        return await this._bloodPressureRepo.update(id, bloodPressureDomainModel);
+    };
+
+    delete = async (id: string): Promise<boolean> => {
+        return await this._bloodPressureRepo.delete(id);
+    };
+
+}

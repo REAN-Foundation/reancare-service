3d33344af46b65b6488962e1f33cd3c6e7b67f97
Kiran Kharade <kiran.kharade@reanfoundation.org>
2021-09-15 17:06:33 +0530

    Added symptom type related types

--- src/domain.types/miscellaneous/system.types.ts
+++ src/domain.types/miscellaneous/system.types.ts
@@ -1,26 +1,39 @@
 
-export type NotThere = null | 'undefined' | '';
+export type NotThere    = null | 'undefined' | '';
 export type Optional<T> = T | NotThere;
 
-export type Gender = 'Male'| 'male' | 'Female' | 'female' | 'Other' | 'other' |'Unknown' | 'unknown' | null;
-export type BloodGroup = 'A+'| 'B+' | 'O+' | 'AB+' | 'A-' | 'B-' |'O-' | 'AB-' | null;
+export type Gender        = 'Male'| 'male' | 'Female' | 'female' | 'Other' | 'other' |'Unknown' | 'unknown' | null;
+export type BloodGroup    = 'A+'| 'B+' | 'O+' | 'AB+' | 'A-' | 'B-' |'O-' | 'AB-' | null;
 export type MaritalStatus = 'Single'| 'Married' | 'Widowed' | 'Divorcee' | 'Live-in' | 'Other' | 'Unknown' | null;
-
-//Generic severities
-export type Severity = 'Low'| 'Medium' | 'High' | 'Critical' | null;
 
 //Visit/Encounter types
 export enum VisitTypes {
     DoctorVisit = 'Doctor visit',
-    LabVisit = 'Lab visit',
-    TeleVisit = 'Tele visit',
-    Unknown = 'Unknown'
+    LabVisit    = 'Lab visit',
+    TeleVisit   = 'Tele visit',
+    Unknown     = 'Unknown'
 }
 
 export enum VisitStates {
-    Started = 'Started',
+    Started    = 'Started',
     InProgress = 'In-progress',
-    Cancelled = 'Cancelled',
-    Completed = 'Completed',
-    Unknown = 'Unknown'
+    Cancelled  = 'Cancelled',
+    Completed  = 'Completed',
+    Unknown    = 'Unknown'
 }
+
+export enum Severity {
+    Low      = 'Low',
+    Medium   = 'Medium',
+    High     = 'High',
+    Critical = 'Critical',
+    Unknown  = 'Unknown',
+}
+
+export const SeverityList: Severity[] = [
+    Severity.Low,
+    Severity.Medium,
+    Severity.High,
+    Severity.Critical,
+    Severity.Unknown
+];

a5e35486fc8277f6eb9a504c5a45c6c551bf6a4e
Kiran Kharade <82871552+kiran-rean@users.noreply.github.com>
2021-09-18 11:50:45 +0530

    Delete src/domain.types/daily.records/CalorieBalance directory

--- src/domain.types/daily.records/CalorieBalance/calorie.balance.domain.model.ts
+++
@@ -1,9 +0,0 @@
-export interface CalorieBalanceDomainModel {
-    id?: string;
-    PersonId: string;
-    PatientUserId?: string;
-    CaloriesConsumed?: number;
-    CaloriesBurned?: number;
-    Unit?: string;
-    RecordDate?: Date;
-}

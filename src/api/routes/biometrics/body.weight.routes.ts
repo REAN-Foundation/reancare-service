f2a07d98c1a7cbc5e02eec95fc30a673fd5407bf
Kiran Kharade <kiran.kharade@reanfoundation.org>
2021-09-18 17:40:19 +0530

    Added requests and tests for body weight

--- src/api/routes/biometrics/body.weight.routes.ts
+++ src/api/routes/biometrics/body.weight.routes.ts
@@ -12,12 +12,9 @@ export const register = (app: express.Application): void => {
     const controller = new BodyWeightController();
 
     router.post('/', authenticator.authenticateClient, authenticator.authenticateUser, controller.create);
-
     router.get('/search', authenticator.authenticateClient, authenticator.authenticateUser, controller.search);
-
     router.get('/by-patient/:patientUserId', authenticator.authenticateClient, authenticator.authenticateUser, controller.getByPatientUserId);
     router.get('/:id', authenticator.authenticateClient, authenticator.authenticateUser, controller.getById);
-
     router.put('/:id', authenticator.authenticateClient, authenticator.authenticateUser, controller.update);
     router.delete('/:id', authenticator.authenticateClient, authenticator.authenticateUser, controller.delete);
     

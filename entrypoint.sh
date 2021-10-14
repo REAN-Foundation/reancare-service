7fa43a31d79a50cfc4b5058ae88ff2e685d5e3be
Kiran Kharade <82871552+kiran-rean@users.noreply.github.com>
2021-10-14 15:34:28 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 # Add config/creds copying here...
 aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/env.config /app/.env.$NODE_ENV
 
-aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/seed_data/internal.clients.seed.json /app/src/seed.data/internal.clients.seed.json
-aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/seed_data/internal.test.users.seed.json /app/src/seed.data/internal.test.users.seed.json
-aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/seed_data/system.admin.seed.json /app/src/seed.data/system.admin.seed.json
+aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/seed_data/internal.clients.seed.json /app/seed.data/internal.clients.seed.json
+aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/seed_data/internal.test.users.seed.json /app/seed.data/internal.test.users.seed.json
+aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/seed_data/system.admin.seed.json /app/seed.data/system.admin.seed.json
 aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/gcp_creds/reancareapi-307085d27fd7.json /app/creds/reancareapi-307085d27fd7.json
 aws s3 cp s3://$S3_CONFIG_BUCKET/reancare_service/gcp_creds/reancare_firebase_creds.json /app/creds/reancare_firebase_creds.json
 

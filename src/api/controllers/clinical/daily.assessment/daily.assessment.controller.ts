2a1b3edaeb6dd93a5b4c114de6454f603c08e012
tabbasum-rean <tabbasum.kolhapure@reanfoundation.org>
2021-11-22 11:10:54 +0530

    Review comment fixes

--- src/api/controllers/clinical/daily.assessment/daily.assessment.controller.ts
+++ src/api/controllers/clinical/daily.assessment/daily.assessment.controller.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Logger } from '../../../../common/logger';
 import { ApiError } from '../../../../common/api.error';
 import { ResponseHandler } from '../../../../common/response.handler';
 import { DailyAssessmentService } from '../../../../services/clinical/daily.assessment/daily.assessment.service';
@@ -31,9 +30,7 @@ export class DailyAssessmentController extends BaseController{
             this.setContext('DailyAssessment.Create', request, response);
 
             const model = await this._validator.create(request);
-            Logger.instance().log(`After validation: $JSON.stringify(model)`);
             const dailyAssessment = await this._service.create(model);
-            Logger.instance().log(`After service: $JSON.stringify(dailyAssessment)`);
 
             if (dailyAssessment == null) {
                 throw new ApiError(400, 'Cannot create record for daily assessment!');

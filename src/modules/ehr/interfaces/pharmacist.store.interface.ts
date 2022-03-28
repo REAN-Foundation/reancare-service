c3d37b4882133f7f588bb90546f375ebb62e6835
vinay-debug <vinay.baranwal@reanfoundation.org>
2022-03-28 12:02:33 +0530

    added update and delete in pharmacist fhir

--- src/modules/ehr/interfaces/pharmacist.store.interface.ts
+++ src/modules/ehr/interfaces/pharmacist.store.interface.ts
@@ -1,11 +1,9 @@
 import { PharmacistDomainModel } from '../../../domain.types/pharmacist/pharmacist.domain.types';
-import { PharmacistSearchFilters } from '../../../domain.types/pharmacist/pharmacist.search.types';
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 export interface IPharmacistStore {
     add(PharmacistDomainModel: PharmacistDomainModel): Promise<any>;
-    search(filter: PharmacistSearchFilters): Promise<any>;
     getById(id: string): Promise<any>;
     update(id: string, updates: PharmacistDomainModel): Promise<any>;
     delete(id: string): Promise<any>;
--- src/modules/ehr/services/pharmacist.store.ts
+++ src/modules/ehr/services/pharmacist.store.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import { IPharmacistStore } from '../interfaces/pharmacist.store.interface';
 import { injectable, inject } from "tsyringe";
 import { PharmacistDomainModel } from '../../../domain.types/pharmacist/pharmacist.domain.types';
-import { PharmacistSearchFilters } from '../../../domain.types/pharmacist/pharmacist.search.types';
 
 ///////////////////////////////////////////////////////////////////
 
@@ -13,10 +12,6 @@ export class PharmacistStore {
 
     add = async (model: PharmacistDomainModel): Promise<any> => {
         return await this._service.add(model);
-    }
-
-    search = async (filter: PharmacistSearchFilters): Promise<any> => {
-        return await this._service.search(filter);
     }
 
     getById = async (id: string): Promise<any> => {

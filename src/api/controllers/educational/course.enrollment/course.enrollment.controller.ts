9036cd639711b64242952f4473f7ea60dd51ebfe
Kiran Kharade <kiran.kharade@reanfoundation.org>
2022-09-15 15:13:30 +0530

    Updated course privileges

--- src/api/controllers/educational/course.enrollment/course.enrollment.controller.ts
+++ src/api/controllers/educational/course.enrollment/course.enrollment.controller.ts
@@ -32,7 +32,7 @@ export class CourseEnrollmentController extends BaseController {
 
     enroll = async (request: express.Request, response: express.Response): Promise<void> => {
         try {
-            
+
             await this.setContext('CourseEnrollment.Enroll', request, response);
 
             const model = await this._validator.enroll(request);
@@ -51,7 +51,7 @@ export class CourseEnrollmentController extends BaseController {
 
     startCourseModule = async (request: express.Request, response: express.Response): Promise<void> => {
         try {
-            
+
             await this.setContext('CourseEnrollment.StartCourseModule', request, response);
 
             const model = await this._validator.startCourseModule(request);
@@ -71,7 +71,7 @@ export class CourseEnrollmentController extends BaseController {
 
     startCourseContent = async (request: express.Request, response: express.Response): Promise<void> => {
         try {
-            
+
             await this.setContext('CourseEnrollment.StartCourseContent', request, response);
 
             const model = await this._validator.startCourseContent(request);

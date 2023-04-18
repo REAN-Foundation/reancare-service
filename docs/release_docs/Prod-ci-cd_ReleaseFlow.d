915ad45be11ae89849288ae1334349b1ae3afa55
rf-opssupport <84499879+rf-opssupport@users.noreply.github.com>
2023-04-18 18:03:29 +0530

    Create Prod-ci-cd_ReleaseFlow.d

---
+++ docs/release_docs/Prod-ci-cd_ReleaseFlow.d
@@ -0,0 +1,27 @@
+## PROD-ci-cd
+
+Prod-ci-cd workflow can be use or trigger by Pushing code into main branch.
+
+### JOBS
+
+These are the jobs used in UAT-ci-cd
+
+#### Publish-Release
+
+* This job will create a new Github release and the versioning will be based on what label developer gave for the pull request.
+* This job uses [release-drafter](https://github.com/release-drafter/release-drafter).
+
+#### Deploy-ECS
+
+* This job uses 'PROD' environment and login to ECR using creds and build a new ECR docker image with image tag using ID of release whcih Publish-Release job created for example ``` reancare:97777323 ```, Then this job will create new version of Amazon ECS task definition with new docker image then it will deploy Amazon ECS task definition using Duplo API.
+* This job uses [docker/build-push-action](https://github.com/marketplace/actions/build-and-push-docker-images).
+
+### Steps To Trigger Workflow
+
+1. Once the Pull Request has been merge into main branch prod-ci-cd workflow will be triggered.
+
+### Conclusion
+
+* This workflow will be triggered after UAT-ci-cd workflow which means whenever a Push event happens on the main branch will trigger Prod-ci-cd workflow
+* Then it will create a new docker image and create a new version of task definition with that image and deploy it.
+* Example of PROD-ci-cd Action ![prod-ci-cd](https://github.com/REAN-Foundation/reancare-service/blob/feature/flow_documentation/assets/images/prod-ci-cd_example.png?raw=true)

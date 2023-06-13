84724c6494bd52c46e66ff741dd5ff6982beddf4
Kiran Kharade <kiran.kharade@reanfoundation.org>
2023-06-13 10:57:12 +0530

    Added code to update awards service for user groups.

--- src/services/community/user.group.service.ts
+++ src/services/community/user.group.service.ts
@@ -10,6 +10,8 @@ import {
 import { uuid } from "../../domain.types/miscellaneous/system.types";
 import { Logger } from "../../common/logger";
 import axios from 'axios';
+import { IUserRepo } from "../../database/repository.interfaces/users/user/user.repo.interface";
+import { ConfigurationManager } from "../../config/configuration.manager";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -29,11 +31,12 @@ export class UserGroupService {
 
     constructor(
             @inject('IUserGroupRepo') private _userGroupRepo: IUserGroupRepo,
+            @inject('IUserRepo') private _userRepo: IUserRepo,
     ) {}
 
     create = async (model: UserGroupCreateDomainModel): Promise<UserGroupDto> => {
         var userGroup = await this._userGroupRepo.create(model);
-        var participantGroup = await this.createParticipantGroupForAwards(userGroup);
+        var participantGroup = await this.createAwardsParticipantGroup(userGroup);
         if (!participantGroup) {
             Logger.instance().log('Unable to create participant group for awards! Rolling back...');
             await this.delete(userGroup.id);
@@ -48,11 +51,31 @@ export class UserGroupService {
     };
 
     getById = async (id: string): Promise<UserGroupDto> => {
-        return await this._userGroupRepo.getById(id);
+        var userGroup = await this._userGroupRepo.getById(id);
+        if (userGroup) {
+            var participantGroup = await this.getAwardsParticipantGroup(userGroup.id);
+            if (!participantGroup) {
+                Logger.instance().log('Unable to retrieve participant group for awards!');
+            }
+            else {
+                userGroup.AwardsParticipantGroup = participantGroup;
+            }
+        }
+        return userGroup;
     };
 
     update = async (id: string, updates: UserGroupUpdateDomainModel): Promise<UserGroupDto> => {
-        return await this._userGroupRepo.update(id, updates);
+        var updatedUserGroup = await this._userGroupRepo.update(id, updates);
+        if (updatedUserGroup) {
+            var participantGroup = await this.updateAwardsParticipantGroup(updatedUserGroup);
+            if (!participantGroup) {
+                Logger.instance().log('Unable to update participant group for awards!');
+            }
+            else {
+                updatedUserGroup.AwardsParticipantGroup = participantGroup;
+            }
+        }
+        return updatedUserGroup;
     };
 
     delete = async (id: string): Promise<boolean> => {
@@ -60,11 +83,25 @@ export class UserGroupService {
     };
 
     addUserToGroup = async (groupId: string, userId: string): Promise<boolean> => {
-        return await this._userGroupRepo.addUserToGroup(groupId, userId);
+        const added = await this._userGroupRepo.addUserToGroup(groupId, userId);
+        if (added) {
+            const addedAsParticipant = await this.addUserToAwardsParticipantGroup(groupId, userId);
+            if (!addedAsParticipant) {
+                Logger.instance().log('Unable to add user to participant group for awards!');
+            }
+        }
+        return added;
     };
 
     removeUserFromGroup = async (groupId: string, userId: string): Promise<boolean> => {
-        return await this._userGroupRepo.removeUserFromGroup(groupId, userId);
+        const removed = await this._userGroupRepo.removeUserFromGroup(groupId, userId);
+        if (removed) {
+            const removedAsParticipant = await this.removeUserFromAwardsParticipantGroup(groupId, userId);
+            if (!removedAsParticipant) {
+                Logger.instance().log('Unable to remove user from participant group for awards!');
+            }
+        }
+        return removed;
     };
 
     getGroupUsers = async (groupId: string): Promise<UserGroupDto[]> => {
@@ -85,7 +122,7 @@ export class UserGroupService {
 
     //#region Private Methods
 
-    createParticipantGroupForAwards = async (model: UserGroupDto): Promise<any> => {
+    createAwardsParticipantGroup = async (model: UserGroupDto): Promise<any> => {
         var participantGroup = null;
         try {
             const clientId = await this.getClientId();
@@ -101,6 +138,31 @@ export class UserGroupService {
             if (response.status === 201) {
                 Logger.instance().log('Successfully triggered award event!');
                 participantGroup = response.data.Data;
+            } else {
+                Logger.instance().error('Unable to trigger award event!', response.status, response.data);
+            }
+        }
+        catch (error) {
+            Logger.instance().log(`${error.message}`);
+        }
+        return participantGroup;
+    };
+
+    updateAwardsParticipantGroup = async (userGroup: UserGroupDto): Promise<any> => {
+        var participantGroup = null;
+        try {
+            const group = await this.getAwardsParticipantGroup(userGroup.id);
+
+            var url = process.env.AWARDS_SERVICE_BASE_URL + '/api/v1/participant-groups/' + group.id;
+            var body = {
+                Name        : userGroup.Name,
+                Description : userGroup.Description,
+                ImageUrl    : userGroup.ImageUrl,
+            };
+            var response = await axios.put(url, body, { headers });
+            if (response.status === 200) {
+                Logger.instance().log('Successfully triggered award event!');
+                var participantGroup = response.data.Data;
             } else {
                 Logger.instance().error('Unable to trigger award event!', response.status, response.data);
             }
@@ -131,6 +193,125 @@ export class UserGroupService {
             Logger.instance().log(`${error.message}`);
         }
         return clientId;
+    };
+
+    getAwardsParticipantGroup = async (userGroupId: string): Promise<any> => {
+        try {
+            //userGroupId is participant-group's referenceId for the awards service
+            var url = process.env.AWARDS_SERVICE_BASE_URL + '/api/v1/participant-groups/by-reference-id/' + userGroupId;
+            var response = await axios.get(url, { headers });
+            if (response.status === 200) {
+                Logger.instance().log('Successfully retrieved participant groups!');
+                const participantGroup = response.data.Data;
+                if (participantGroup != null) {
+                    return participantGroup;
+                }
+            } else {
+                Logger.instance().error('Unable to retrieve participant groups!', response.status, response.data);
+            }
+        }
+        catch (error) {
+            Logger.instance().log(`${error.message}`);
+        }
+        return null;
+    };
+
+    getOrCreateParticipant = async (userId: uuid): Promise<any> => {
+        try {
+            var url = process.env.AWARDS_SERVICE_BASE_URL + '/api/v1/participants/by-reference-id/' + userId;
+            var response = await axios.get(url, { headers });
+            if (response.status === 200) {
+                Logger.instance().log('Successfully retrieved participant!');
+                const participant = response.data.Data;
+                if (participant != null) {
+                    return participant;
+                }
+            } else {
+                const user = await this._userRepo.getById(userId);
+                const clientId = await this.getClientId();
+                const fileResourceId = user.Person.ImageResourceId;
+                var profileImageUrl: string | null | undefined = null;
+                if (fileResourceId) {
+                    profileImageUrl = ConfigurationManager.BaseUrl() + '/api/v1/file-resources/' + fileResourceId + '/download';
+                }
+                url = process.env.AWARDS_SERVICE_BASE_URL + '/api/v1/participants';
+                var body = {
+                    clientId        : clientId,
+                    ReferenceId     : userId,
+                    Prefix          : user.Person.Prefix,
+                    FirstName       : user.Person.FirstName,
+                    LastName        : user.Person.LastName,
+                    Email           : user.Person.Email,
+                    Phone           : user.Person.Phone,
+                    Gender          : user.Person.Gender,
+                    BirthDate       : user.Person.BirthDate,
+                    ProfileImageUrl : profileImageUrl,
+                    OnboardingDate  : Date.now(),
+                };
+                response = await axios.post(url, body, { headers });
+                if (response.status === 201) {
+                    Logger.instance().log('Successfully created participant!');
+                    const participant = response.data.Data;
+                    if (participant != null) {
+                        return participant;
+                    }
+                }
+                else {
+                    Logger.instance().error('Unable to create participant!', response.status, response.data);
+                }
+            }
+            return null;
+        }
+        catch (error) {
+            Logger.instance().log(`${error.message}`);
+        }
+        return null;
+    };
+
+    addUserToAwardsParticipantGroup = async (groupId: uuid, userId: uuid): Promise<boolean> => {
+        var added = false;
+        try {
+            const participant = await this.getOrCreateParticipant(userId);
+            if (!participant) {
+                Logger.instance().log('Unable to create participant for awards!');
+                return false;
+            }
+            var url = process.env.AWARDS_SERVICE_BASE_URL + '/api/v1/participant-groups/' + groupId + '/participants/' + participant.id;
+            var response = await axios.post(url, {}, { headers });
+            if (response.status === 200) {
+                Logger.instance().log('Successfully added user to participant group!');
+                added = true;
+            } else {
+                Logger.instance().error('Unable to add user to participant group!', response.status, response.data);
+            }
+        }
+        catch (error) {
+            Logger.instance().log(`${error.message}`);
+        }
+        return added;
+    };
+
+    removeUserFromAwardsParticipantGroup = async (groupId: uuid, userId: uuid): Promise<boolean> => {
+        var removed = false;
+        try {
+            const participant = await this.getOrCreateParticipant(userId);
+            if (!participant) {
+                Logger.instance().log('Unable to create participant for awards!');
+                return false;
+            }
+            var url = process.env.AWARDS_SERVICE_BASE_URL + '/api/v1/participant-groups/' + groupId + '/participants/' + participant.id;
+            var response = await axios.delete(url, { headers });
+            if (response.status === 200) {
+                Logger.instance().log('Successfully removed user from participant group!');
+                removed = true;
+            } else {
+                Logger.instance().error('Unable to remove user from participant group!', response.status, response.data);
+            }
+        }
+        catch (error) {
+            Logger.instance().log(`${error.message}`);
+        }
+        return removed;
     };
 
     //#endregion

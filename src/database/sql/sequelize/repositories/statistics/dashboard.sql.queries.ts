e98e057682145675c4b966913ea9dd098f8da784
Dattatraya-Londhe <londhedn@gmail.com>
2024-02-19 12:03:50 +0530

    Update dashboard sql queries

--- src/database/sql/sequelize/repositories/statistics/dashboard.sql.queries.ts
+++ src/database/sql/sequelize/repositories/statistics/dashboard.sql.queries.ts
@@ -49,3 +49,201 @@ WHERE UserId IN
         WHERE IsTestUser = false
     )
 GROUP BY OSType`;
+
+export const queryAllYear =
+`SELECT DISTINCT YEAR(CreatedAt) AS years
+FROM persons
+order by years asc`;
+
+export const queryYearWiseUserCount =
+`SELECT DISTINCT YEAR(CreatedAt) AS years, COUNT(*) AS totalUsers
+FROM users
+WHERE IsTestUser = false
+GROUP BY years
+ORDER BY years asc`;
+
+export const queryYearWiseDeviceDetail =
+`SELECT DISTINCT YEAR(CreatedAt) AS years, OSType, COUNT(*) AS totalUsers
+FROM user_device_details
+WHERE UserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years, OSType
+ORDER BY years asc`;
+
+export const queryUserByAge =
+`SELECT Age, COUNT(*) as totalCount
+FROM persons
+WHERE id IN 
+    (
+        SELECT PersonId
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY Age`;
+
+export const queryUserByGender =
+`SELECT Gender, COUNT(*) as totalCount
+FROM persons
+WHERE id IN 
+    (
+        SELECT PersonId
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY Gender`;
+
+export const queryUserMarritalStatus =
+`SELECT MaritalStatus, COUNT(*) as totalCount
+FROM patient_health_profiles
+WHERE PatientUserId IN 
+    (
+        SELECT id
+        FROM reanadmintestdata4.users
+        WHERE IsTestUser = false
+    )
+GROUP BY MaritalStatus`;
+
+export const queryUserByMajorAilment =
+`SELECT MajorAilment, COUNT(*) as totalCount
+FROM patient_health_profiles
+WHERE PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY MajorAilment`;
+
+export const queryTobaccoSmokers =
+`SELECT COUNT(*) AS tobaccoUserCount
+FROM patient_health_profiles
+WHERE TobaccoQuestionAns = true AND PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )`;
+
+export const queryYearWiseTobaccoSmokers =
+`SELECT DISTINCT YEAR(CreatedAt) AS years, COUNT(*) AS tobaccoUserCount
+FROM patient_health_profiles
+WHERE TobaccoQuestionAns = true AND PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years
+ORDER BY years ASC`;
+
+export const queryHeavyDrinkers =
+`SELECT COUNT(*) AS drinkerUserCount
+FROM patient_health_profiles
+WHERE IsDrinker = true AND DrinkingSeverity = 'High' AND PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    );`;
+
+export const queryYearWiseHeavyDrinkers =
+`SELECT DISTINCT YEAR(CreatedAt) AS years, COUNT(*) AS drinkerUserCount
+FROM patient_health_profiles
+WHERE IsDrinker = true AND DrinkingSeverity = 'High' AND PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years
+ORDER BY years ASC`;
+
+export const querySubstanceAbuse =
+`SELECT COUNT(*) AS substanceAbuseUserCount
+FROM patient_health_profiles
+WHERE SubstanceAbuse = true AND PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )`;
+
+export const queryYearWiseSubstanceAbuse =
+`SELECT DISTINCT YEAR(CreatedAt) AS years, COUNT(*) AS substanceAbuseUserCount
+FROM patient_health_profiles
+WHERE SubstanceAbuse = true AND PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years
+ORDER BY years ASC`;
+
+export const queryNotAddited =
+`SELECT COUNT(*) AS notAddedUserCount
+FROM patient_health_profiles
+WHERE 
+    (SubstanceAbuse = false AND IsDrinker = false AND (TobaccoQuestionAns = false OR TobaccoQuestionAns = null))
+    AND
+    PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )`;
+
+export const queryYearWiseNotAddited =
+`SELECT DISTINCT YEAR(CreatedAt) as years, COUNT(*) AS notAddedUserCount
+FROM patient_health_profiles
+WHERE 
+    (SubstanceAbuse = false AND IsDrinker = false AND (TobaccoQuestionAns = false OR TobaccoQuestionAns = null))
+    AND
+    PatientUserId IN 
+        (
+            SELECT id
+            FROM users
+            WHERE IsTestUser = false
+        )
+GROUP BY years
+ORDER BY years ASC`;
+
+export const queryYearWiseGenderDetails =
+`SELECT DISTINCT YEAR(CreatedAt) AS years , Gender, COUNT(*) as totalCount
+FROM persons
+WHERE id IN 
+    (
+        SELECT PersonId
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years, Gender
+ORDER BY years asc`;
+
+export const queryYearWiseMaritalDetails =
+`SELECT  DISTINCT YEAR(CreatedAt) AS years,  MaritalStatus, COUNT(*) as totalCount
+FROM patient_health_profiles
+WHERE PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years, MaritalStatus
+ORDER BY years asc`;
+
+export const queryYearWiseMajorAilmentDistributionDetails =
+`SELECT DISTINCT YEAR(CreatedAt) as years, MajorAilment, COUNT(*) as totalCount
+FROM patient_health_profiles
+WHERE PatientUserId IN 
+    (
+        SELECT id
+        FROM users
+        WHERE IsTestUser = false
+    )
+GROUP BY years, MajorAilment
+ORDER BY years ASC`;

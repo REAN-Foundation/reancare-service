19198c48bed24844068606b89d3c89b5a8a71bfb
Dattatraya-Londhe <londhedn@gmail.com>
2024-03-16 13:01:53 +0530

    Updated health report settings permissions

--- src/api/users/patient/statistics/health.report.setting/health.report.setting.routes.ts
+++ src/api/users/patient/statistics/health.report.setting/health.report.setting.routes.ts
@@ -9,9 +9,9 @@ export const register = (app: express.Application): void => {
     const router = express.Router();
     const controller = new HealthReportSettingController();
 
-    router.post('/', auth('User.Patient.Patient.Statistics.Report.Create', true), controller.create);
-    router.get('/:patientUserId/settings', auth('User.Patient.Patient.Statistics.Report.GetByUserId', true), controller.getByUserId);
-    router.put('/:patientUserId', auth('User.Patient.Patient.Statistics.Report.UpdateByUserId', true), controller.updateByUserId);
+    router.post('/', auth('User.Patient.Statistics.CreateReportSettings'), controller.create);
+    router.get('/:patientUserId/settings', auth('User.Patient.Statistics.GetReportSettingsByUserId'), controller.getByUserId);
+    router.put('/:patientUserId', auth('User.Patient.Statistics.UpdateReportSettingsByUserId'), controller.updateByUserId);
  
     app.use('/api/v1/patient-statistics/health-report', router);
 };

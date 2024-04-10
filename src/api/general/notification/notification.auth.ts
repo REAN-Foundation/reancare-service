91a47845bdff2506cd447754cb15f149a6663f41
Kiran Kharade <kiran.kharade@reanfoundation.org>
2024-04-10 18:10:11 +0530

    Updtated notification auth.

---
+++ src/api/general/notification/notification.auth.ts
@@ -0,0 +1,15 @@
+import {
+    AuthOptions,
+    RequestType,
+    ResourceOwnership,
+    ActionScope,
+    DefaultAuthOptions
+} from '../../../auth/auth.types';
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+export class NotificationAuth {
+
+    static _baseContext = 
+
+}

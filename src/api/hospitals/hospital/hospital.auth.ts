fdd91214c5b24602c706a4b7a571c799eb9226b7
Dattatraya-Londhe <londhedn@gmail.com>
2024-04-13 16:14:48 +0530

    Added authorization for hospital.

---
+++ src/api/hospitals/hospital/hospital.auth.ts
@@ -0,0 +1,64 @@
+import {
+    AuthOptions,
+    RequestType,
+    ResourceOwnership,
+    ActionScope,
+    DefaultAuthOptions
+} from '../../../auth/auth.types';
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+export class HospitalAuth {
+
+    static _baseContext = `Hospital`;
+
+    static create: AuthOptions = {
+        ...DefaultAuthOptions,
+        Context     : `${this._baseContext}.Create`,
+        Ownership   : ResourceOwnership.Tenant,
+        ActionScope : ActionScope.Tenant,
+        RequestType : RequestType.CreateOne,
+    };
+
+    static getHospitalsForHealthSystem: AuthOptions = {
+        ...DefaultAuthOptions,
+        Context     : `${this._baseContext}.GetHospitalsForHealthSystem`,
+        Ownership   : ResourceOwnership.Tenant,
+        ActionScope : ActionScope.Tenant,
+        RequestType : RequestType.GetMany,
+    };
+
+    static search: AuthOptions = {
+        ...DefaultAuthOptions,
+        Context             : `${this._baseContext}.Search`,
+        Ownership           : ResourceOwnership.Tenant,
+        ActionScope         : ActionScope.Tenant,
+        RequestType         : RequestType.Search,
+        CustomAuthorization : true,
+    };
+
+    static getById: AuthOptions = {
+        ...DefaultAuthOptions,
+        Context     : `${this._baseContext}.GetById`,
+        Ownership   : ResourceOwnership.Tenant,
+        ActionScope : ActionScope.Tenant,
+        RequestType : RequestType.GetOne,
+    };
+
+    static update: AuthOptions = {
+        ...DefaultAuthOptions,
+        Context     : `${this._baseContext}.Update`,
+        Ownership   : ResourceOwnership.Tenant,
+        ActionScope : ActionScope.Tenant,
+        RequestType : RequestType.UpdateOne,
+    };
+
+    static delete: AuthOptions = {
+        ...DefaultAuthOptions,
+        Context     : `${this._baseContext}.Delete`,
+        Ownership   : ResourceOwnership.Tenant,
+        ActionScope : ActionScope.Tenant,
+        RequestType : RequestType.DeleteOne,
+    };
+
+}

046ee6b8ab534b4d55c3ff6f8180ae51dac239c7
Dattatraya-Londhe <londhedn@gmail.com>
2024-04-14 13:28:33 +0530

    Added authorization on wearable.

--- src/api/devices/wearables/wearable.device.details.routes.ts
+++ src/api/devices/wearables/wearable.device.details.routes.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import { auth } from '../../../auth/auth.handler';
 import { WearableDeviceDetailsController } from './wearable.device.details.controller';
+import { WearableAuth } from './wearable.auth';
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -10,23 +11,23 @@ export const register = (app: express.Application): void => {
     const router = express.Router();
     const controller = new WearableDeviceDetailsController();
 
-    router.post('/', auth('Devices.Wearables.Create'), controller.create);
-    router.get('/search', auth('Devices.Wearables.Search'), controller.search);
-    router.get('/patients/:patientUserId', auth('Devices.Wearables.GetUserWearables'), controller.getPatientWearableDeviceDetails);
-    router.get('/:id', auth('Devices.Wearables.GetById'), controller.getById);
-    router.put('/:id', auth('Devices.Wearables.Update'), controller.update);
-    router.delete('/:id', auth('Devices.Wearables.Delete'), controller.delete);
+    router.post('/', auth(WearableAuth.create), controller.create);
+    router.get('/search', auth(WearableAuth.search), controller.search);
+    router.get('/patients/:patientUserId', auth(WearableAuth.getUserWearables), controller.getPatientWearableDeviceDetails);
+    router.get('/:id', auth(WearableAuth.getById), controller.getById);
+    router.put('/:id', auth(WearableAuth.update), controller.update);
+    router.delete('/:id', auth(WearableAuth.delete), controller.delete);
 
     app.use('/api/v1/devices/wearables', router);
 
     // Obsolute routes. Will be discontinued in future
-    const obsoluteRouter = express.Router();
-    obsoluteRouter.post('/', auth('Devices.Wearables.Create'), controller.create);
-    obsoluteRouter.get('/search', auth('Devices.Wearables.Search'), controller.search);
-    obsoluteRouter.get('/patients/:patientUserId', auth('Devices.Wearables.GetUserWearables'), controller.getPatientWearableDeviceDetails);
-    obsoluteRouter.get('/:id', auth('Devices.Wearables.GetById'), controller.getById);
-    obsoluteRouter.put('/:id', auth('Devices.Wearables.Update'), controller.update);
-    obsoluteRouter.delete('/:id', auth('Devices.Wearables.Delete'), controller.delete);
+    // const obsoluteRouter = express.Router();
+    // obsoluteRouter.post('/', auth('Devices.Wearables.Create'), controller.create);
+    // obsoluteRouter.get('/search', auth('Devices.Wearables.Search'), controller.search);
+    // obsoluteRouter.get('/patients/:patientUserId', auth('Devices.Wearables.GetUserWearables'), controller.getPatientWearableDeviceDetails);
+    // obsoluteRouter.get('/:id', auth('Devices.Wearables.GetById'), controller.getById);
+    // obsoluteRouter.put('/:id', auth('Devices.Wearables.Update'), controller.update);
+    // obsoluteRouter.delete('/:id', auth('Devices.Wearables.Delete'), controller.delete);
 
-    app.use('/api/v1/wearable-device-details', obsoluteRouter);
+    // app.use('/api/v1/wearable-device-details', obsoluteRouter);
 };

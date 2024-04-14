b47aecc62ebcaca97cd4c8650ed3e2548a7a4761
Dattatraya-Londhe <londhedn@gmail.com>
2024-04-14 13:09:06 +0530

    Added authorization for course content.

---
+++ src/api/educational/learning/course.content/course.content.auth.ts
@@ -0,0 +1,72 @@
+import {
+    AuthOptions,
+    RequestType,
+    ResourceOwnership,
+    ActionScope,
+    DefaultAuthOptions
+} from '../../../../auth/auth.types';
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+export class CourseContentAuth {
+
+static _baseContext = `Educational.Learning.CourseContent`;
+
+static create: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context     : `${this._baseContext}.Create`,
+    Ownership   : ResourceOwnership.Tenant,
+    ActionScope : ActionScope.Tenant,
+    RequestType : RequestType.CreateOne,
+};
+
+static update: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context     : `${this._baseContext}.Update`,
+    Ownership   : ResourceOwnership.Tenant,
+    ActionScope : ActionScope.Tenant,
+    RequestType : RequestType.UpdateOne,
+};
+
+static delete: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context     : `${this._baseContext}.Delete`,
+    Ownership   : ResourceOwnership.Tenant,
+    ActionScope : ActionScope.Tenant,
+    RequestType : RequestType.DeleteOne,
+};
+
+static getById: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context     : `${this._baseContext}.GetById`,
+    Ownership   : ResourceOwnership.Tenant,
+    ActionScope : ActionScope.Public,
+    RequestType : RequestType.GetOne,
+};
+
+static getContentsForCourse: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context     : `${this._baseContext}.GetContentsForCourse`,
+    Ownership   : ResourceOwnership.Tenant,
+    ActionScope : ActionScope.Public,
+    RequestType : RequestType.GetOne,
+};
+
+static getContentsForLearningPath: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context     : `${this._baseContext}.GetContentsForLearningPath`,
+    Ownership   : ResourceOwnership.Tenant,
+    ActionScope : ActionScope.Tenant,
+    RequestType : RequestType.GetOne,
+};
+
+static search: AuthOptions = {
+    ...DefaultAuthOptions,
+    Context             : `${this._baseContext}.Search`,
+    Ownership           : ResourceOwnership.Tenant,
+    ActionScope         : ActionScope.Tenant,
+    RequestType         : RequestType.Search,
+    CustomAuthorization : true,
+};
+
+}

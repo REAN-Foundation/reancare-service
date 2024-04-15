abadda4e784b913995a7b8982a1ea06351cd8f0e
Dattatraya-Londhe <londhedn@gmail.com>
2024-04-15 16:40:04 +0530

    Updated Auth options for user.

--- src/auth/auth.types.ts
+++ src/auth/auth.types.ts
@@ -51,14 +51,14 @@ export interface AuthOptions {
 }
 
 export const DefaultAuthOptions: AuthOptions = {
-    Context            : null,
-    ActionScope        : ActionScope.Owner,
-    Ownership          : ResourceOwnership.Owner,
-    RequestType        : RequestType.Custom,
-    ResourceIdName     : 'id',
-    ClientAppAuth      : true,
-    CustomAuthorization: false,
-    AlternateAuth      : false,
-    SignupOrSignin     : false,
-    OptionalUserAuth   : false,
+    Context             : null,
+    ActionScope         : ActionScope.Owner,
+    Ownership           : ResourceOwnership.Owner,
+    RequestType         : RequestType.Custom,
+    ResourceIdName      : 'id',
+    ClientAppAuth       : true,
+    CustomAuthorization : false,
+    AlternateAuth       : false,
+    SignupOrSignin      : false,
+    OptionalUserAuth    : false,
 };

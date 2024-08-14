849c25b6ee17a6fb1555882e24106f0f888e6715
Dattatraya-Londhe <londhedn@gmail.com>
2024-08-14 15:00:09 +0530

    Typo fix.

--- src/api/router.ts
+++ src/api/router.ts
@@ -87,7 +87,7 @@ import { register as registerHealthSystemRoutes } from './hospitals/health.syste
 import { register as registerHospitalRoutes } from './hospitals/hospital/hospital.routes';
 import { register as registerDailyStatisticsRoutes } from './statistics/daily.statistics/daily.statistics.routes';
 import { register as registerTenantFeatureSettingRoutes } from './tenant/settings/tenant.settings.routes';
-import { register as registerFollowUpCancellationRoutes } from './followup.cancellation/followup.cancellation.routes';
+import { register as registerFollowUpCancellationRoutes } from './tenant/followups/cancellations/followup.cancellation.routes';
 
 ////////////////////////////////////////////////////////////////////////////////////
 

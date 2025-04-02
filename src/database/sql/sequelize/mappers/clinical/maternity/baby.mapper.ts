a31d13b332050857e1333ee41b4e504990ea0dbc
MadhuraAnarase <madhuraa712@gmail.com>
2025-04-02 09:49:26 +0530

    Baby end to end API

---
+++ src/database/sql/sequelize/mappers/clinical/maternity/baby.mapper.ts
@@ -0,0 +1,24 @@
+import { BabyDto } from '../../../../../../domain.types/clinical/maternity/baby/baby.dto';
+import BabyModel from '../../../models/clinical/maternity/baby.model';
+
+///////////////////////////////////////////////////////////////////////////////////
+
+export class BabyMapper {
+
+    static toDto = (baby: BabyModel): BabyDto => {
+        if (!baby) {
+            return null;
+        }
+        const dto: BabyDto = {
+            id                 : baby.id,
+            DeliveryId         : baby.DeliveryId,
+            PatientUserId      : baby.PatientUserId,
+            WeightAtBirthGrams : baby.WeightAtBirthGrams,
+            Gender             : baby.Gender,
+            Status             : baby.Status,
+            ComplicationId     : baby.ComplicationId,
+        };
+        return dto;
+    };
+
+}

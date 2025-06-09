83256c354f4dfb882a9e58ccf7d67ea5f80f2ece
Dattatraya-Londhe <londhedn@gmail.com>
2025-06-09 11:21:56 +0530

    Code cleanup.

--- src/services/clinical/assessment/assessment.template.service.ts
+++ src/services/clinical/assessment/assessment.template.service.ts
@@ -12,11 +12,15 @@ import { AssessmentNodeSearchFilters } from "../../../domain.types/clinical/asse
 import { AssessmentNodeSearchResults } from "../../../domain.types/clinical/assessment/assessment.node.search.types";
 import { Logger } from "../../../common/logger";
 import { ApiError } from "../../../common/api.error";
+import { AssessmentTemplateHelper } from "./assessment.template.helper";
+import { Injector } from "../../../startup/injector";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 @injectable()
 export class AssessmentTemplateService {
+
+    assessmentTemplateHelper = Injector.Container.resolve(AssessmentTemplateHelper);
 
     constructor(
         @inject('IAssessmentTemplateRepo') private _assessmentTemplateRepo: IAssessmentTemplateRepo,
@@ -64,8 +68,9 @@ export class AssessmentTemplateService {
 
     public import = async (model: any): Promise<AssessmentTemplateDto> => {
         let template: CAssessmentTemplate = model as CAssessmentTemplate;
-        template = this.copyAssessmentTemplate(template);
-        template.Title = `${template?.Title} - Copy`;
+        template = this.assessmentTemplateHelper.copyAssessmentTemplate(template);
+        const serachResult = await this.search({ Title: template.Title });
+        template.Title = serachResult.TotalCount > 0 ? `${template?.Title}-Copy ${serachResult.TotalCount + 1}` : `${template?.Title}-Copy`;
         return await this.addTemplate(template);
     };
 
@@ -206,113 +211,6 @@ export class AssessmentTemplateService {
         return await this._assessmentHelperRepo.deleteOption(optionId);
     };
 
-    copyAssessmentTemplate = (originalTemplate: CAssessmentTemplate) : CAssessmentTemplate => {
-        const copyTemplate = originalTemplate;
-
-        const nodeCodeMap: Record<string, string> = {};
-        const optionCodeMap: Record<string, string> = {};
-        const conditionCodeMap: Record<string, string> = {};
-
-        copyTemplate.ProviderAssessmentCode = Helper.generateDisplayId();
-
-        const newTemplateDisplayCode = Helper.generateDisplayCode('AssessmtTmpl');
-        copyTemplate.DisplayCode = newTemplateDisplayCode;
-
-        for (const node of copyTemplate.Nodes) {
-            const oldCode = node.DisplayCode;
-            const isRNode = node.DisplayCode.startsWith('RNode');
-            const prefix = this.getPrefixFromType(node.NodeType, isRNode);
-            if (!prefix) {
-                throw new ApiError(400, `Invalid node type: ${node.NodeType}`);
-            }
-            const newCode = Helper.generateDisplayCode(prefix);
-            nodeCodeMap[oldCode] = newCode;
-            node.DisplayCode = newCode;
-        }
-
-        if (originalTemplate.RootNodeDisplayCode && nodeCodeMap[originalTemplate.RootNodeDisplayCode]) {
-            copyTemplate.RootNodeDisplayCode = nodeCodeMap[originalTemplate.RootNodeDisplayCode];
-        }
-
-        for (const node of copyTemplate.Nodes) {
-            if (node.NodeType === AssessmentNodeType.NodeList && node.ChildrenNodeDisplayCodes?.length > 0) {
-                node.ChildrenNodeDisplayCodes = node.ChildrenNodeDisplayCodes.map((oldChildCode: string) =>
-                    nodeCodeMap[oldChildCode] || oldChildCode
-                );
-            }
-
-            if (node.ParentNodeDisplayCode && nodeCodeMap[node.ParentNodeDisplayCode]) {
-                node.ParentNodeDisplayCode = nodeCodeMap[node.ParentNodeDisplayCode];
-            }
-            
-            if (node.NodeType === AssessmentNodeType.Question) {
-                const questionNode = node as CAssessmentQuestionNode;
-                if (questionNode.Options?.length) {
-                    questionNode.Options = this.copyOptions(questionNode.Options, optionCodeMap);
-                }
-
-                if (questionNode.Paths && Array.isArray(questionNode.Paths)) {
-                    questionNode.Paths = this.copyConditionCodes(
-                        questionNode.Paths,
-                        questionNode.DisplayCode,
-                        conditionCodeMap
-                    );
-                }
-            }
-        }
-
-        return copyTemplate;
-    };
-
-    private copyOptions = (
-        options: CAssessmentQueryOption[],
-        optionCodeMap: Record<string, string>
-    ): CAssessmentQueryOption[] => {
-        return options.map(option => {
-            const newCode = `${Helper.generateDisplayCode('QNode')}:Option#${option.Sequence}`;
-            optionCodeMap[option.DisplayCode] = newCode;
-            option.DisplayCode = newCode;
-            return option;
-        });
-    };
-
-    private copyConditionCodes = (
-        conditions: CAssessmentPathCondition[],
-        parentCode: string,
-        conditionCodeMap: Record<string, string>
-    ): CAssessmentPathCondition[] => {
-        return conditions.map((condition) => {
-            const newCode = Helper.generateDisplayCode('Condition');
-            if (condition.DisplayCode) {
-                conditionCodeMap[condition.DisplayCode] = newCode;
-            }
-            condition.DisplayCode = newCode;
-
-            if (condition.Children && condition.Children.length > 0) {
-                condition.Children = this.copyConditionCodes(
-                    condition.Children,
-                    parentCode,
-                    conditionCodeMap,
-                );
-            }
-
-            return condition;
-        });
-    };
-
-    private getPrefixFromType = (type: string, isRNode: boolean = false): string => {
-        switch (type) {
-            case AssessmentNodeType.Message:
-                return 'MNode';
-            case AssessmentNodeType.Question:
-                return 'QNode';
-            case AssessmentNodeType.NodeList:
-                return isRNode ? 'RNode' : 'LNode';
-            default:
-                return null;
-        }
-    };
-
     public setupBasicAssessmentTemplate = async (tenantId: uuid): Promise<void> => {
         const displayCode = "BasicAssessmentTemplate";
 
@@ -330,7 +228,7 @@ export class AssessmentTemplateService {
         let templateObj: CAssessmentTemplate = await this.readTemplateObjToExport(assessmentTemplateId);
         templateObj.TenantId = tenantId;
 
-        templateObj = this.copyAssessmentTemplate(templateObj);
+        templateObj = this.assessmentTemplateHelper.copyAssessmentTemplate(templateObj);
         const template = await this._assessmentHelperRepo.addTemplate(templateObj);
 
         if (!template) {
